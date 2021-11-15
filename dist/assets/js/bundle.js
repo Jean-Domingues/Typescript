c840a46a920ef73e21080184338d8e30b93ad745
Jean Domingues <jeandomingues752@gmail.com>
2021-11-15 20:23:15 -0300

    configurando webpack

---
+++ dist/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/webpack/index.ts ***!
+  \******************************/
+
+console.log('Bom dia');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
